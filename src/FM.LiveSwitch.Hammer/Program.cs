e7c4d079d396b87f79ab553064b184b937263f19
Anton Venema <anton.venema@frozenmountain.com>
2021-01-14 13:15:28 -0800

    support for environment variables

--- src/FM.LiveSwitch.Hammer/Program.cs
+++ src/FM.LiveSwitch.Hammer/Program.cs
@@ -4,6 +4,9 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -37,7 +40,7 @@ namespace FM.LiveSwitch.Hammer
                 ClusterTestOptions,
                 LoadTestOptions,
                 ScanTestOptions
-            >(args);
+            >(AppendEnvironmentVariables(args));
 
             result.MapResult(
                 (ClusterTestOptions options) =>
@@ -120,6 +123,88 @@ namespace FM.LiveSwitch.Hammer
         private static void LogException(Exception ex)
         {
             Console.Error.WriteLine($"{ex.Message} {ex.InnerException?.Message}".Trim());
+        }
+
+        private static string[] AppendEnvironmentVariables(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return args;
+            }
+
+            var verb = args[0];
+            if (!TryGetOptions(verb, out var options))
+            {
+                return args;
+            }
+
+            var newArgs = new List<string>(args);
+            foreach (var unusedOption in FilterOptions(args, options))
+            {
+                var value = Environment.GetEnvironmentVariable($"{Assembly.GetExecutingAssembly().GetName().Name.ToUpperInvariant()}_{verb.ToUpperInvariant()}_{unusedOption.LongName.ToUpperInvariant()}");
+                if (value != null)
+                {
+                    Console.Error.WriteLine($"Environment variable discovered matching --{unusedOption.LongName} option.");
+                    newArgs.Add($"--{unusedOption.LongName}={value}");
+                }
+            }
+            return newArgs.ToArray();
+        }
+
+        private static string Environmentalize(string name)
+        {
+            return name.ToUpperInvariant().Replace("-", "_").Replace(".", "_");
+        }
+
+        private static bool TryGetOptions(string verb, out OptionAttribute[] options)
+        {
+            foreach (var type in Assembly.GetExecutingAssembly().GetTypes().Where(type => !type.IsAbstract))
+            {
+                var verbAttribute = type.GetCustomAttributes<VerbAttribute>().FirstOrDefault();
+                if (verbAttribute != null)
+                {
+                    if (verbAttribute.Name == verb)
+                    {
+                        options = type.GetProperties()
+                            .Select(property => property.GetCustomAttributes<OptionAttribute>().FirstOrDefault())
+                            .Where(option => option != null).ToArray();
+                        return true;
+                    }
+                }
+            }
+
+            options = null;
+            return false;
+        }
+
+        private static OptionAttribute[] FilterOptions(string[] args, OptionAttribute[] options)
+        {
+            var usedLongNames = new HashSet<string>();
+            var usedShortNames = new HashSet<string>();
+
+            foreach (var arg in args)
+            {
+                if (arg.StartsWith("--"))
+                {
+                    var longName = arg.Substring(2);
+                    if (longName.Contains('='))
+                    {
+                        longName = longName.Substring(0, longName.IndexOf('='));
+                    }
+                    usedLongNames.Add(longName);
+                }
+                else if (arg.StartsWith("-"))
+                {
+                    var shortName = arg.Substring(1);
+                    if (shortName.Contains('='))
+                    {
+                        shortName = shortName.Substring(0, shortName.IndexOf('='));
+                    }
+                    usedShortNames.Add(shortName);
+                }
+            }
+
+            return options.Where(option => !usedLongNames.Contains(option.LongName) && !usedShortNames.Contains(option.ShortName)).ToArray();
         }
     }
 }
