ae07fd71854cb10b443f801a8f3ba4fb7beea4fa
Hardeep Sidhu <hardeep.sidhu@frozenmountain.com>
2021-06-21 10:21:49 -0700

    Added the "--sdk-log-level" option to enable/configure LiveSwitch SDK logging.

---
+++ src/FM.LiveSwitch.Hammer/SdkLogProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FM.LiveSwitch.Hammer
+{
+    class SdkLogProvider : LogProvider
+    {
+        public SdkLogProvider(LogLevel level)
+        {
+            Level = level;
+        }
+
+        protected override void DoLog(LogEvent logEvent)
+        {
+            Console.Error.WriteLine(GenerateLogLine(logEvent));
+        }
+    }
+}
