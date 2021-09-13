d8973f1c74a0aff8ecae7f3dbe0ed2cc13aa7c1b
Hardeep Sidhu <hardeep.sidhu@frozenmountain.com>
2021-09-13 11:01:47 -0700

    Skip a scan test scenario if no automatic ice servers are found for the scenario unless it's the host test.

---
+++ src/FM.LiveSwitch.Hammer/NoIceServersException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FM.LiveSwitch.Hammer
+{
+    [Serializable]
+    public class NoIceServersException : Exception
+    {
+        public NoIceServersException(string message)
+            : base(message)
+        { }
+    }
+}
